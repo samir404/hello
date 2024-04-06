1b8b3da40170a1afa52e4b931cc2fc72f666ff48
samir404 <166224106+samir404@users.noreply.github.com>
2024-04-06 18:09:25 +0545

    Add files via upload

---
+++ pro.js
@@ -0,0 +1,25 @@
+gsap.to("h1",{
+  x:100,
+  rotate:360,
+  duration:3,
+  delay:0.6
+})
+
+gsap.from("h2",{
+  x:10,
+  rotate:360,
+  duration:3,
+  delay:0.6
+})
+gsap.from("h3",{
+  x:10,
+  rotate:360,
+  duration:3,
+  delay:0.6
+})
+gsap.to(".aaa",{
+  y:1000,
+
+  duration:10,
+  delay:4
+})
